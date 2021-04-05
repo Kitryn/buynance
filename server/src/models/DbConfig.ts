aaa44bdf8dca1f8aa794ca3baa02ccd1de4e1681
Kitryn <snowfennard@gmail.com>
2021-04-05 20:07:51 +0800

    configure db to prepend test_ to name if in dev env

--- server/src/models/DbConfig.ts
+++ server/src/models/DbConfig.ts
@@ -2,7 +2,8 @@ import path from 'path'
 import Database from 'better-sqlite3'
 
 module.exports = function InitDb(): Database.Database {
-    const db = new Database(path.resolve(process.env.DBPATH, process.env.DBNAME))
+    const DBNAME = process.env.NODE_ENV === 'production' ? process.env.DBNAME : `test_${process.env.DBNAME}`
+    const db = new Database(path.resolve(process.env.DBPATH, DBNAME))
     db.pragma('busy_timeout = 200')
     db.pragma('foreign_keys = ON')
     return db
