f51959421df02808180adc2bdf3c9bfc4b5262ef
Kitryn <snowfennard@gmail.com>
2021-04-05 18:45:38 +0800

    Created table schema for storing factories, pairs, and token contract data

--- server/src/models/DbConfig.ts
+++ server/src/models/DbConfig.ts
@@ -4,5 +4,6 @@ import Database from 'better-sqlite3'
 module.exports = function InitDb(): Database.Database {
     const db = new Database(path.resolve(process.env.DBPATH, process.env.DBNAME))
     db.pragma('busy_timeout = 200')
+    db.pragma('foreign_keys = ON')
     return db
 }
--- server/src/models/Pairs.ts
+++ server/src/models/Pairs.ts
@@ -6,5 +6,34 @@ export class Pairs {
     
     constructor(db: Database.Database) {
         this.db = db
+        const createFactoryTable = this.db.prepare('CREATE TABLE IF NOT EXISTS factories(\
+            name STRING UNIQUE,\
+            contract_address STRING UNIQUE NOT NULL,\
+            factory_id INTEGER PRIMARY KEY)')
+        
+        const createTokenTable = this.db.prepare('CREATE TABLE IF NOT EXISTS tokens(\
+            decimals INTEGER NOT NULL,\
+            name STRING NOT NULL,\
+            symbol STRING NOT NULL,\
+            contract_address STRING UNIQUE NOT NULL)')
+        
+        const createPairsTable = this.db.prepare('CREATE TABLE IF NOT EXISTS pairs(\
+            decimals INTEGER NOT NULL,\
+            token0 STRING NOT NULL,\
+            token1 STRING NOT NULL,\
+            factory_address STRING NOT NULL,\
+            FOREIGN KEY (factory_address)\
+                REFERENCES factories (contract_address),\
+            FOREIGN KEY (token0)\
+                REFERENCES tokens (contract_address),\
+            FOREIGN KEY (token1)\
+                REFERENCES tokens (contract_address))')
+        
+        const initialiseDb = this.db.transaction(() => {
+            createFactoryTable.run()
+            createTokenTable.run()
+            createPairsTable.run()
+        })
+        initialiseDb()
     }
 }
