9e4a407a6f42b19fae30531ccd5563759bf14400
Kitryn <snowfennard@gmail.com>
2021-04-07 00:50:53 +0800

    Sync mainnet sashimiswap data

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,6 @@
-import assert from 'assert'
-
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
-
 
 const dbConn = require('./models/DbConfig')()
 import { PairsModel } from './models/PairsModel'
@@ -12,30 +9,35 @@ import { ChainData } from './chain_interface/ChainData'
 import { PairAccount } from './chain_interface/PairAccount'
 import { TokenAccount } from './chain_interface/TokenAccount'
 import { ethers } from 'ethers'
-
-
+import { BaseFactories, BaseTokens } from './addresses'
+import { Factory } from './types'
 
 // const db = require('./models/DbConfig')()
 // const pairsDb = new Pairs(db)
-const mainnet_PancakeFactoryAddress = '0xBCfCcbde45cE874adCB698cC183deBcF17952812'
-const mainnet_PancakeRouterAddress = '0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F'
 const mainnet_BAKE_BUSD = '0xE2D1B285d83efb935134F644d00FB7c943e84B5B'
 const mainnet_BUSD = '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56'
 const mainnet_BAKE = '0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5'
 
-const testnet_PancakeFactoryAddress = '0x6725F303b657a9451d8BA641348b6761A6CC7a17'
 
 
 async function main() {
-    // const db = new PairsModel(dbConn)
-    // db.addFactory('Pancakeswap', mainnet_PancakeFactoryAddress)
-    // const provider = new ethers.providers.JsonRpcProvider(RpcUrl.MAINNET)
+    console.log(`Running on ${process.env.NETWORK}`)
     const RPC_URL = process.env.NETWORK === 'MAINNET' ? process.env.RPC_URL : process.env.RPC_URL_TESTNET
     
     const chainData = new ChainData(RPC_URL)
     chainData.loadAllFactories()
-    chainData.registerNewFactory('Pancakeswap', mainnet_PancakeFactoryAddress, mainnet_PancakeRouterAddress, 0.002)
-    await chainData.sync(mainnet_PancakeFactoryAddress)
+
+    let Pancakeswap_address: string = ''
+    // load factories from hardcoded factories since if it already exists, nothing happens
+    for (const factoryKey in BaseFactories) {
+        const factoryInfo: Factory = BaseFactories[factoryKey]
+        chainData.registerNewFactory(factoryInfo.name, factoryInfo.contract_address, factoryInfo.router_address, factoryInfo.fee)
+        
+        // Usage for test purposes
+        if (factoryInfo.name === 'Pancakeswap') Pancakeswap_address = factoryInfo.contract_address
+    }
+    if (Pancakeswap_address === '') throw new Error('Pancakeswap address not found')
+    await chainData.sync('0x1DaeD74ed1dD7C9Dabbe51361ac90A69d851234D')
 }
 
 main()
