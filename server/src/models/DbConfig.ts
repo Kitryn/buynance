1f9cde6d764aa4483067a145baa54dc15f19dbcd
Kitryn <snowfennard@gmail.com>
2021-04-07 00:13:45 +0800

    Change db file name based on mainnet vs testnet

--- server/src/models/DbConfig.ts
+++ server/src/models/DbConfig.ts
@@ -2,7 +2,7 @@ import path from 'path'
 import Database from 'better-sqlite3'
 
 module.exports = function InitDb(): Database.Database {
-    const DBNAME = process.env.NODE_ENV === 'production' ? process.env.DBNAME : `test_${process.env.DBNAME}`
+    const DBNAME = process.env.NETWORK === 'MAINNET' ? process.env.DBNAME : `TESTNET_${process.env.DBNAME}`
     const db = new Database(path.resolve(process.env.DBPATH, DBNAME))
     db.pragma('busy_timeout = 200')
     db.pragma('foreign_keys = ON')
