5efa2d3ab12e0ae8410f949bd190e0c60dfeda57
Kitryn <snowfennard@gmail.com>
2021-04-08 22:49:46 +0800

    Add sample WETH (customised, untested, do not deploy on mainnet!)

---
+++ contracts/TestInfrastructure/wETH.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.3;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+// NOT CANONICAL! DO NOT DEPLOY ON MAINNET
+contract WETH is ERC20 {
+    event Deposit(address indexed dst, uint wad);
+    event Withdrawal(address indexed src, uint wad);
+
+    constructor() ERC20("Wrapped Ether", "WETH") {}
+
+    receive() external payable {
+        deposit();
+    }
+
+    function deposit() public payable {
+        _mint(msg.sender, msg.value);
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    function withdraw(uint wad) public {
+        require(balanceOf(msg.sender) >= wad);
+        _burn(msg.sender, wad);
+        payable(msg.sender).transfer(wad);
+        emit Withdrawal(msg.sender, wad);
+    }
+}
