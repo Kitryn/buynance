6ff558f8b297785f6e802a2b790acac6090fbd12
Kitryn <snowfennard@gmail.com>
2021-04-09 12:23:20 +0800

    Clean up truffle

--- migrations/2_deploy_contracts.js
+++
@@ -1,9 +0,0 @@
-const WETH = artifacts.require('WETH')
-const ILMoney = artifacts.require('ILMoney')
-
-module.exports = function(deployer) {
-  deployer.deploy(WETH)
-  deployer.deploy(ILMoney)
-  // deployer.link(ConvertLib, MetaCoin)
-  // deployer.deploy(MetaCoin)
-}
