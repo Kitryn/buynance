950887f979d061c9ca8b94ed9ee6bbf9a575fd8c
Adriel Chua <34438587+acdx@users.noreply.github.com>
2021-04-10 12:06:04 +0100

    update

--- contracts/main.sol
+++ contracts/main.sol
@@ -10,21 +10,15 @@ import '../interfaces/IUniswapV2Router01.sol';
 import '../libraries/SafeMath.sol';
 import '../libraries/UniswapV2Library.sol';
 
-// Need to install uniswap libraries.
-
 //Inputs:
 //1. Factory DEX 1
 //2. Router DEX 1
 //3. Factory DEX 2
 //4. Router DEX 2
-//5. Address token A
-//6. Address token B 
-//7. 'True price' token A (price from one of the DEX)
-//8. 'True price' token B (price from one of the DEX)
 
 contract ExampleSwapToPrice {
     using SafeMath for uint256;
-    
+
     // initiate address for router and factory for DEX 1 and DEX 2
     IUniswapV2Router01 public immutable router1;
     address public immutable factory1;
@@ -39,43 +33,49 @@ contract ExampleSwapToPrice {
         router2 = router2_;
     }
 
-    // swaps an amount of either token such that the trade is profit-maximizing, given an external true price
-    // true price is expressed in the ratio of token A to token B
-    // caller must approve this contract to spend whichever token is intended to be swapped
-    // 'True price' here should refer to True price of the 2nd exchange
-    // Would probably be easier if we did the profit maximizing out of the SC (save gas as well, maybe?)
     function swapToPrice(
         address tokenA,
         address tokenB,
-        uint256 truePriceTokenA,
-        uint256 truePriceTokenB,
         uint256 maxSpendTokenA,
         uint256 maxSpendTokenB,
         address to,
         uint256 deadline
     ) public {
-        // true price is expressed as a ratio, so both values must be non-zero
-        require(truePriceTokenA != 0 && truePriceTokenB != 0, "ExampleSwapToPrice: ZERO_PRICE");
+
         // caller can specify 0 for either if they wish to swap in only one direction, but not both
         require(maxSpendTokenA != 0 || maxSpendTokenB != 0, "ExampleSwapToPrice: ZERO_SPEND");
-
+        
+        //Initialize variables we need 
         bool aToB;
         uint256 amountIn;
+        uint256 amountOut;
+        
         {
+            (uint256 truePriceTokenA,uint256 truePriceTokenB) = UniswapV2Library.getReserves(factory2, tokenA, tokenB);
+
+            // true price is expressed as a ratio, so both values must be non-zero
+            require(truePriceTokenA != 0 && truePriceTokenB != 0, "ExampleSwapToPrice: ZERO_PRICE");
+            // caller can specify 0 for either if they wish to swap in only one direction, but not both
+            require(maxSpendTokenA != 0 || maxSpendTokenB != 0, "ExampleSwapToPrice: ZERO_SPEND");
+
             (uint256 reserveA, uint256 reserveB) = UniswapV2Library.getReserves(factory1, tokenA, tokenB);
             (aToB, amountIn) = UniswapV2LiquidityMathLibrary.computeProfitMaximizingTrade(
                 truePriceTokenA, truePriceTokenB,
                 reserveA, reserveB
             );
+            if (aToB) {
+                amountOut = UniswapV2Library.getAmountOut(amountIn, reserveA, reserveB);
+            } else {
+                amountOut = UniswapV2Library.getAmountOut(amountIn, reserveB, reserveA);
+            }
+            // spend up to the allowance of the token in
+            uint256 maxSpend = aToB ? maxSpendTokenA : maxSpendTokenB;
+            if (amountIn > maxSpend) {
+                amountIn = maxSpend;
+            }
         }
 
         require(amountIn > 0, 'ExampleSwapToPrice: ZERO_AMOUNT_IN');
-
-        // spend up to the allowance of the token in
-        uint256 maxSpend = aToB ? maxSpendTokenA : maxSpendTokenB;
-        if (amountIn > maxSpend) {
-            amountIn = maxSpend;
-        }
 
         address tokenIn = aToB ? tokenA : tokenB;
         address tokenOut = aToB ? tokenB : tokenA;
@@ -94,21 +94,14 @@ contract ExampleSwapToPrice {
             to,
             deadline
         );
-        // Get amount out
-        if (aToB) {
-            uint amountOut = UniswapV2Library.getAmountOut(amountIn, reserveA, reserveB);
-        } else {
-            uint amountOut = UniswapV2Library.getAmountOut(amountIn, reserveB, reserveA);
-        }
-        
-        
+
         // Swapping on 2nd exchange at price of 2nd exchange('True Price')
         address[] memory path_2 = new address[](2);
         path_2[0] = tokenOut;
         path_2[1] = tokenIn;
-        uint amountIn_2 = amountOut;
+        //uint amountIn_2 = 5;
         router2.swapExactTokensForTokens(
-            amountIn_2,
+            amountOut,
             0, // amountOutMin: we can skip computing this number because the math is tested
             path_2,
             to,
